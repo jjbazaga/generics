b0209ba7ef8ac7819ae56a7114bd52ac883253c3
jjbazaga <jjbazaga@gmail.com>
2023-07-28 13:57:52 -0300

    Initial commit

---
+++ src/Apllication/Program.java
@@ -0,0 +1,28 @@
+package Apllication;
+
+import entities.PrintService;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        PrintService ps = new PrintService();
+
+        System.out.print("How many values? ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            Integer value = sc.nextInt();
+            ps.addValue(value);
+        }
+
+        ps.print();
+        Integer x = ps.first();
+        System.out.println("First: " + x);
+
+        sc.close();
+    }
+}
